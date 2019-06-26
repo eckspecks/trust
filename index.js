88e816698569d7f0e89f7bc7d7e1f54afbd0f06d
eckspecks <52166004+eckspecks@users.noreply.github.com>
2019-06-26 17:31:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,38 +6,50 @@ var set = new Set();
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function(socket){
-  console.log('a user connected');
+app.get('/teacher.html', function(req, res){
+  res.sendFile(__dirname + '/teacher.html');
 });
+app.get('/student.html', function(req, res){
+  res.sendFile(__dirname + '/student.html');
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-io.on('connection', function(socket){
 
-  //gets code from teacher, adds code to the set of codes     
-  socket.on('message', function(msg){
-    console.log('message: ' + msg);
-    set.add(msg);
-  });
-    
-    
-      
-});
 
 io.on('connection', function(socket){
+    
+    console.log('a user connected');
+    
+    socket.on('message', function(msg){
+        
+        console.log('message: ' + msg);
+        set.add(msg);
+}); 
+    
     
   //gets code from the player, compares it with code from teacher    
   socket.on('code', function(theCode){
+      
      console.log('TheCode' + theCode);
     if(set.has(theCode)){
+        
         //if code from player is found in the set, return success
         socket.emit('code',"success");
         console.log("success");
+        
     }else{
+        
         //if not, return failure
         socket.emit('code',"failure");
         console.log("failure");
+        
     }
   });
+    
+ socket.on('nickname',function(theNickname){
+     console.log('thenickname' + theNickname);
+     socket.emit('nickname',theNickname);
+});
 });
