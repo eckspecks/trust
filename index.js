9ebc91574c663c19ead0499056114d9450565f2a
eckspecks <52166004+eckspecks@users.noreply.github.com>
2019-06-26 22:33:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -24,24 +24,29 @@ io.on('connection', function(socket){
     console.log('a user connected');
     
     socket.on('message', function(msg){
+        //teacher creates a new roomNumber
         socket.join(roomnum);
         console.log("success " + roomnum);
-
+        
         roomnum++;
         console.log('message: ' + msg);
+        
+        //adds the room code to array
         array.push(msg);
 }); 
     
     
   //gets code from the player, compares it with code from teacher    
-  socket.on('code', function(theCode){
+  socket.on('code', function(playersCode){
       
-    if(array.includes(theCode)){
+    if(array.includes(playersCode)){
         
-        socket.join(array.indexOf(theCode));
+        //gets the room number the player is in based on index of the code player inputted
+        var roomNumber = array.indexOf(playersCode);
+        socket.join(roomNumber);
         //if code from player is found in the set, return success
-        socket.emit('code',"success " + array.indexOf(theCode));
-        console.log("success! Room number:" + array.indexOf(theCode) + " " + theCode);
+        socket.emit('code',"success " + roomNumber);
+        console.log("success! Room number:" + roomNumber + " " + playersCode);
         
     }else{
         
@@ -53,7 +58,20 @@ io.on('connection', function(socket){
   });
     
  socket.on('nickname',function(theNickname){
+     
      console.log('Nickname: ' + theNickname);
+     
+     //sends the nickname of each player in the teacher's room to the teacher
      io.to(theNickname[theNickname.length -1]).emit('nickname',theNickname.slice(0,-2));
 });
+    
+    
+ socket.on('readyToPlay',function(gameCode){
+     
+     var roomNumber = array.indexOf(gameCode.split(" ")[1]);
+
+     //sends the nickname of each player in the teacher's room to the teacher
+     io.to(roomNumber).emit('readyToPlay',"play");
+});    
+    
 });
