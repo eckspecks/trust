2a5a2da5fcfe0506d4691d36d1a0cc8bcb69b4ec
eckspecks <52166004+eckspecks@users.noreply.github.com>
2019-08-03 16:53:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -181,8 +181,8 @@ io.on('connection', function(socket){
      
 
          http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-      resp.on('data', function(ip) {
-        console.log("My public IP address is: " + ip);
+        resp.on('data', function(ip) {
+        io.to(socket.id).emit('ip',ip);
       });
     });
      
