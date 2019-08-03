6bcf4e427d4f2671c277ee3d2e886e3c2a76b3d1
eckspecks <52166004+eckspecks@users.noreply.github.com>
2019-08-03 16:57:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,49 @@ function indexOfArray(array, item) {
     return -1;
 }
 
+function arrayBufferToString(buffer){
+    var byteArray = new Uint8Array(buffer);
+    var str = "", cc = 0, numBytes = 0;
+    for(var i=0, len = byteArray.length; i<len; ++i){
+        var v = byteArray[i];
+        if(numBytes > 0){
+            //2 bit determining that this is a tailing byte + 6 bit of payload
+            if((cc&192) === 192){
+                //processing tailing-bytes
+                cc = (cc << 6) | (v & 63);
+            }else{
+                throw new Error("this is no tailing-byte");
+            }
+        }else if(v < 128){
+            //single-byte
+            numBytes = 1;
+            cc = v;
+        }else if(v < 192){
+            //these are tailing-bytes
+            throw new Error("invalid byte, this is a tailing-byte")
+        }else if(v < 224){
+            //3 bits of header + 5bits of payload
+            numBytes = 2;
+            cc = v & 31;
+        }else if(v < 240){
+            //4 bits of header + 4bit of payload
+            numBytes = 3;
+            cc = v & 15;
+        }else{
+            //UTF-8 theoretically supports up to 8 bytes containing up to 42bit of payload
+            //but JS can only handle 16bit.
+            throw new Error("invalid encoding, value out of range")
+        }
+
+        if(--numBytes === 0){
+            str += String.fromCharCode(cc);
+        }
+    }
+    if(numBytes){
+        throw new Error("the bytes don't sum up");
+    }
+    return str;
+}
 io.on('connection', function(socket){
     
 
@@ -182,7 +225,8 @@ io.on('connection', function(socket){
 
          http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
         resp.on('data', function(ip) {
-        io.to(socket.id).emit('ip',ip);
+        var stringIp = arrayBufferToString(ip);
+        io.to(socket.id).emit('ip',stringIp);
       });
     });
      
